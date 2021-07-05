a86a142712345be57b9027a33904ab38a5f4d269
asmabatttikhi <asmaaalbattikhi@gmail.com>
2021-07-05 16:25:57 +0300

    aboutmegame

---
+++ js/app.js
@@ -0,0 +1,61 @@
+'use strict';
+
+let username=prompt("hello whats your name ? ");
+alert=('hello '+username + ',lets ask some qusitions');
+
+
+let myname=prompt('am i asma ?').toLowerCase();
+    //console.log(myname);
+if(myname===true){
+    //console.log('correct');
+    alert('correct');
+}else{
+    //console.log('false');
+    alert('false');
+}
+
+let alien=prompt('am i an alian ?').toLowerCase();
+    //console.log(alien);
+    if(alien===true){
+        //console.log('correct');
+        alert('correct');
+    }else{
+        //console.log('false');
+        alert('false');
+    }
+
+
+let food=prompt('do i eat cat food ?').toLowerCase();
+    //console.log(food);
+    if(alien===true){
+        //console.log('correct');
+        alert('correct');
+    }else{
+        //console.log('false');
+        alert('false');
+    }
+
+let rich=prompt('am i going to be rich ?').toLowerCase();
+   //console.log(rich);
+   if(rich===true){
+    //console.log('correct');
+    alert('correct');
+}else{
+    //console.log('false');
+    alert('false');
+}
+
+let fly=prompt('can i fly?').toLowerCase();
+   // console.log(fly);
+   if(fly===true){
+    //console.log('correct');
+    alert('correct');
+}else{
+    //console.log('false');
+    alert('false');
+}
+
+
+
+
+
