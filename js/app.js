c759d443668721fb18bd63066c231ec0556630c9
asmabatttikhi <asmaaalbattikhi@gmail.com>
2021-07-07 08:57:03 +0300

    class 3

--- js/app.js
+++ js/app.js
@@ -8,62 +8,71 @@ let myname=prompt('am i asma ?');
     //console.log(myname);
     myname=myname.toLowerCase();
    
-if(myname==='yes'){
+if(myname==='yes'|| myname==='y'){
     //console.log('correct');
     alert('correct');
-}else{
+}else if (myname==='no'|| myname==='n'){
     //console.log('false');
     alert('false');
+} else {
+    alert('please only write yes/y or no/n');
 }
+
+
 
 
 let alien=prompt('am i an alian ?').toLowerCase();
     //console.log(alien);
     alien=alien.toLowerCase();
     
-    if(alien==='yes'){
+    if(alien==='yes'||alien==='y'){
         //console.log('correct');
         alert('correct');
-    }else{
+    }else if ((alien==='no'||alien==='n' )){
         //console.log('false');
         alert('false');
+    } else {
+        alert('please only write yes/y or no/n');
     }
 
 
 let food=prompt('do i eat cat food ?').toLowerCase();
     //console.log(food);
     food=food.toLowerCase();
-    if(alien==='yes'){
+    if(food==='yes'||food==='y'){
         //console.log('correct');
         alert('correct');
-    }else{
+    }else if ((food==='no'||food==='n')){
         //console.log('false');
         alert('false');
+    }else{
+        alert('please only write yes/y or no/n');
     }
 
 let rich=prompt('am i going to be rich ?').toLowerCase();
    //console.log(rich);
    rich=rich.toLowerCase();
-   if(rich==='yes'){
+   if(rich==='yes' ||rich==='y'){
     //console.log('correct');
     alert('correct');
-}else{
+}else if(rich==='no' ||rich==='n'){
     //console.log('false');
     alert('false');
+}else{
+    alert('please only write yes/y or no/n');
 }
 
 let fly=prompt('can i fly?').toLowerCase();
    // console.log(fly);
    fly=fly.toLowerCase();
-   if(fly==='yes'){
+   if(fly==='yes'||fly==='y'){
     //console.log('correct');
     alert('correct');
-}else{
+}else if ((fly==='no'||fly==='n')){
     //console.log('false');
     alert('false');
+} else {
+    alert('please only write yes/y or no/n');
 }
-
-
-
 
 
